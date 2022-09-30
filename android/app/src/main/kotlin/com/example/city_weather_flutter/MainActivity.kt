adf4ae003e578fdfe757a2ef832b7b52d3c36c05
Nandir <nandir.b@nmma.co>
2022-09-30 14:49:53 +0800

    init

---
+++ android/app/src/main/kotlin/com/example/city_weather_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.city_weather_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = ThemeData();
+
+    return MaterialApp(
+      title: 'City weather',
+      theme: theme.copyWith(
+        colorScheme: theme.colorScheme.copyWith(
+          primary: Colors.blueAccent,
+          secondary: Colors.amberAccent,
+        ),
+      ),
+      home: const MyHomePage(title: 'City weather'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+  final String description = 'Weather in your city';
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    // 1
+    return Scaffold(
+      // 2
+      appBar: AppBar(
+        title: Text(widget.description),
+      ),
+      // 3
+      body: SafeArea(
+        // TODO: Replace child: Container()
+        // 4
+        child: Container(),
+      ),
+    );
+  }
+
+  // TODO: Add buildRecipeCard() here
+}
